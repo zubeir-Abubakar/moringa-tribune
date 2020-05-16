beef2de362ca8552846f3de10d31a8a00a2ef039
zubeir <zubkayare@gmail.com>
2020-05-16 17:02:41 +0300

    adding registrations on admin page

--- news/admin.py
+++ news/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Editor,Article,tags
+
+admin.site.register(Editor)
+admin.site.register(Article)
+admin.site.register(tags)
 
 # Register your models here.
