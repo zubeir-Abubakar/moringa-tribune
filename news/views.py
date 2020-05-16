efadb5f702aa3d75ea2f7bc40037bb7ddc69d3bb
zubeir <zubkayare@gmail.com>
2020-05-16 17:42:36 +0300

    adding texts

--- news/views.py
+++ news/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render,redirect
 from django.http  import HttpResponse,Http404
 import datetime as dt
+from .models import Article
+
 
 
 # Create your views here.
@@ -32,18 +34,24 @@ def convert_dates(dates):
     day = days[day_number]
     return day
 
-def past_days_news(request,past_date):
+def past_days_news(request, past_date):
     try:
-            # Converts data from the string Url
-        date = dt.datetime.strptime(past_date,'%Y-%m-%d').date()
+        # Converts data from the string Url
+        date = dt.datetime.strptime(past_date, '%Y-%m-%d').date()
     except ValueError:
         # Raise 404 error when ValueError is thrown
         raise Http404()
         assert False
 
     if date == dt.date.today():
-        return redirect(news_of_day)
+        return redirect(news_today)
 
-    return render(request, 'all-news/past-news.html', {"date": date})
+    news = Article.days_news(date)
+    return render(request, 'all-news/past-news.html',{"date": date,"news":news})
+
+def news_today(request):
+    date = dt.date.today()
+    news = Article.todays_news()
+    return render(request, 'all-news/today-news.html', {"date": date,"news":news})
 
 
