5e1b783ecc8c808ff5826926fb0e0fb4fa06b428
zubeir <zubkayare@gmail.com>
2020-05-16 17:27:33 +0300

    adding texts

--- news/models.py
+++ news/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+import datetime as dt
+from django.contrib.auth.models import User
+from tinymce.models import HTMLField
 
 # Create your models here.
 class Editor(models.Model):
@@ -9,18 +12,36 @@ class Editor(models.Model):
 
     def __str__(self):
         return self.first_name
-
+    class meta:
+        ordering =['name']
+    
     def save_editor(self):
         self.save()
+
 
 class tags(models.Model):
     name = models.CharField(max_length =30)
 
     def __str__(self):
         return self.name
+
 class Article(models.Model):
     title = models.CharField(max_length =60)
-    post = models.TextField()
-    editor = models.ForeignKey(Editor)
+    post = HTMLField()
+    editor = models.ForeignKey(User,on_delete=models.CASCADE)
     tags = models.ManyToManyField(tags)
     pub_date = models.DateTimeField(auto_now_add=True)
+    article_image = models.ImageField(upload_to = 'articles/')
+
+    def __str__(self):
+        return self.title
+    @classmethod
+    def todays_news(cls):
+        today = dt.date.today()
+        news = cls.objects.filter(pub_date__date = today)
+        return news
+    
+    @classmethod
+    def days_news(cls,date):
+        news = cls.objects.filter(pub_date__date = date)
+        return news
