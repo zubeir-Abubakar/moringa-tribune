b13c9572d7780258bae7a61a6e53dce177d79d52
zubeir <zubkayare@gmail.com>
2020-05-16 17:26:43 +0300

    adding articals on admin page

--- news/admin.py
+++ news/admin.py
@@ -1,8 +1,13 @@
+
 from django.contrib import admin
 from .models import Editor,Article,tags
+# Register your models here.
+
+class ArticleAdmin(admin.ModelAdmin):
+    filter_horizontal =('tags',)
 
 admin.site.register(Editor)
-admin.site.register(Article)
+admin.site.register(Article,ArticleAdmin)
 admin.site.register(tags)
 
 # Register your models here.
